54fcc16d4847de80da8af6e5a32cd3ba209c9f84
meters-flower <751705693@qq.com>
2017-02-14 15:12:49 +0800

    development
    数据库连接  注册响应

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var path = require('path');
+var session = require('express-session');
+var MongoStore  = require('connect-mongo')(session);
+var settings = require('./setting');
+var flash = require('connect-flash');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -9,9 +13,15 @@ var index = require('./routes/index');
 
 var app = express();
 
+var debug = require('debug')('my-application'); // debug模块  
+app.set('port', process.env.PORT || 3000); // 设定监听端口
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+app.use(flash());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,6 +29,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({
+    // db: settings.db
+    url: 'mongodb://localhost/blog'
+  })
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -41,4 +63,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
+//启动监听  
+var server = app.listen(app.get('port'), function() {  
+  debug('Express server listening on port ' + server.address().port);  
+});  
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');  //用来生成散列值来加密密码
+
+var User = require('../models/user.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: '主页' });
+});
+
+router.get('/reg', function(req, res) {
+  res.render('reg', { title: '注册' });
+});
+
+router.post('/reg', function(req, res) {
+    var name =  req.body.username,
+        password = req.body.password,
+        password_re = req.body.repwd;
+    //检验两次输入的密码是否一致
+    if(password != password_re) {
+        req.flash('error', '两次输入的密码不一致！');
+        return res.redirect('/reg');
+    }
+    //生成密码的md5值
+    var md5 = crypto.createHash('md5'),
+        pwd = md5.update(password).digest('hex');
+    var newUser = new User({
+        name: name,
+        password:pwd
+    });
+    //检查用户是否存在
+    User.get(newUser.name, function(err, user) {
+        if(user) {
+            req.flash('error', '用户已存在！');
+            return res.redirect('/reg');
+        }
+        //如果用户不存在则新增用户
+        newUser.save(function(err, user) {
+            if(err) {
+                req.flash('error', err);
+                return res.redirect('/reg'); //注册失败返回注册页
+            }
+            req.session.user = user; //用户信息存入session
+            req.flash('success', '注册成功！');
+            res.redirect('/'); //注册成功返回主页
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { title: '登录' });
+});
+
+router.post('/login', function(req, res) {
+});
+
+router.get('/post', function(req, res) {
+  res.render('post', { title: '发表' });
+});
+
+router.get('/post', function(req, res) {
+});
+
+router.get('/logout', function(req, res) {
 });
 
 module.exports = router;
