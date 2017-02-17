1a12d16e27c3b09002ed236b54304604b01a1cea
meters-flower <751705693@qq.com>
2017-02-17 13:53:53 +0800

    article、edit、remove
    文章详情、编辑、删除功能

--- models/post.js
+++ models/post.js
@@ -18,6 +18,7 @@ Post.prototype.save = function(callback) {
         year: date.getFullYear(),
         month: date.getFullYear() + "-" + (date.getMonth() + 1),
         day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+        minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
     };
     //要存入数据库的文档
     var post = {
@@ -50,7 +51,7 @@ Post.prototype.save = function(callback) {
 };
 
 //读取文章及相关信息
-Post.get = function(name, callback) {
+Post.getAll = function(name, callback) {
     //打开数据库
     mongodb.open(function(err, db) {
         if(err) {
@@ -77,10 +78,130 @@ Post.get = function(name, callback) {
 
                 //解析markdown为html
                 docs.forEach(function(doc) {
-                    doc.post = markdown.toHTML(doc.post);
+                    if(doc.post){ 
+                        doc.post = markdown.toHTML(doc.post);
+                    }
                 });
                 
                 callback(null, docs); //成功!以数组形式返回查询结果              
+            });
+        })
+    });
+}
+
+//获取一篇文章
+Post.getOne = function(name, day, title, callback) {
+    //打开数据库
+    mongodb.open(function(err, db) {
+        if(err) {
+            return callback(err); 
+        }
+        //读取posts集合
+        db.collection('posts', function(err, collection) {
+            if(err) {
+                mongodb.close();
+                return callback(err);
+            }  
+            //根据用户名、发表日期及文章名进行查询
+            collection.findOne({
+                "name": name,
+                "time.day": day,
+                "title": title
+            }, function(err, doc) {
+                mongodb.close();
+                if(err) {
+                    return callback(err); 
+                } 
+                //解析markdown为html
+                if(doc.post) {
+                    doc.post = markdown.toHTML(doc.post);                    
+                }
+                callback(null, doc); //返回查询的一篇文章
+            });
+        })
+    });
+}
+
+//返回原始发表的内容（markdown格式）
+Post.edit = function(name, day, title, callback) {
+    mongodb.open(function(err, db) {
+        if(err) {
+            return callback(err); 
+        }
+        db.collection('posts', function(err, collection) {
+            if(err) {
+                mongodb.close();
+                return callback(err);
+            }  
+            //根据用户名、发表日期及文章名进行查询
+            collection.findOne({
+                "name": name,
+                "time.day": day,
+                "title": title
+            }, function(err, doc) {
+                mongodb.close();
+                if(err) {
+                    return callback(err); 
+                } 
+                callback(null, doc); //返回查询的一篇文章
+            });
+        })
+    });
+}
+
+//更新一篇文章及其相关信息
+Post.update = function(name, day, title, post, callback) {
+    mongodb.open(function(err, db) {
+        if(err) {
+            return callback(err); 
+        }
+        db.collection('posts', function(err, collection) {
+            if(err) {
+                mongodb.close();
+                return callback(err);
+            }  
+            //更新文章内容
+            collection.update({
+                "name": name,
+                "time.day": day,
+                "title": title
+            }, {
+                $set: {post:post}
+            },function(err) {
+                mongodb.close();
+                if(err) {
+                    return callback(err); 
+                } 
+                callback(null); 
+            });
+        })
+    });
+}
+
+//删除一篇文章
+Post.remove = function(name, day, title, callback) {
+    mongodb.open(function(err, db) {
+        if(err) {
+            return callback(err); 
+        }
+        db.collection('posts', function(err, collection) {
+            if(err) {
+                mongodb.close();
+                return callback(err);
+            }  
+            //根据用户名、日期和标题查找并删除一篇文章
+            collection.remove({
+                "name": name,
+                "time.day": day,
+                "title": title
+            }, {
+                w: 1
+            },function(err) {
+                mongodb.close();
+                if(err) {
+                    return callback(err); 
+                } 
+                callback(null); 
             });
         })
     });
--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ function checkNotLogin(req, res, next ) {
 
 /* 路由控制 */
 router.get('/', function(req, res) {
-    Post.get(null, function(err, posts) {
+    Post.getAll(null, function(err, posts) {
         if(err) {
             posts = [];
         }
@@ -186,4 +186,92 @@ router.post('/upload', function(req, res) {
     });
 });
 
+router.get('/u/:name', function(req, res) {
+    //检查用户是否存在
+    User.get(req.params.name, function(err, user) {
+        if(!user) {
+            req.flash('error', '用户不存在！');
+            return res.redirect('/');
+        }
+        //查询并返回该用户的所有文章
+        Post.getAll(user.name, function(err, posts) {
+            if(err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('user', { 
+                title: user.name,
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success'.toString()),
+                error: req.flash('error'.toString())
+            });
+        });
+    });    
+});
+
+router.get('/u/:name/:day/:title', function(req, res) {
+    //查询并返回文章详细页面
+    Post.getOne(req.params.name, req.params.day, req.params.title, function(err, post) {
+        if(err) {
+            req.flash('error', err);
+            return res.redirect('/');
+        }
+        res.render('article', { 
+            title: req.params.title,
+            post: post,
+            user: req.session.user,
+            success: req.flash('success'.toString()),
+            error: req.flash('error'.toString())
+        });
+    });  
+});
+
+router.get('/edit/:name/:day/:title', checkLogin);
+router.get('/edit/:name/:day/:title', function(req, res) {
+    var currentUser = req.session.user;
+    //文章编辑
+    Post.edit(currentUser.name, req.params.day, req.params.title, function(err, post) {
+        if(err) {
+            req.flash('error', err);
+            return res.redirect('back');
+        }
+        res.render('edit', { 
+            title: '编辑',
+            post: post,
+            user: req.session.user,
+            success: req.flash('success'.toString()),
+            error: req.flash('error'.toString())
+        });
+    });  
+});
+
+router.post('/edit/:name/:day/:title', checkLogin);
+router.post('/edit/:name/:day/:title', function(req, res) {
+    var currentUser = req.session.user;
+    //文章修改
+    Post.update(currentUser.name, req.params.day, req.params.title, req.body.post, function(err) {
+        var url = '/u/' + req.params.name + '/' + req.params.day + '/' + req.params.title;
+        if(err) {
+            req.flash('error', err);
+            return res.redirect(url);
+        }
+        req.flash('success', '修改成功!');
+        res.redirect(url); //成功！返回文章页
+    });  
+});
+
+router.get('/remove/:name/:day/:title', checkLogin);
+router.get('/remove/:name/:day/:title', function(req, res) {
+    var currentUser = req.session.user;
+    //文章删除
+    Post.remove(currentUser.name, req.params.day, req.params.title, function(err) {
+        if(err) {
+            req.flash('error', err);
+            return res.redirect('back');
+        }
+        req.flash('success', '删除成功!');
+        res.redirect('/'); 
+    });  
+});
 module.exports = router;
